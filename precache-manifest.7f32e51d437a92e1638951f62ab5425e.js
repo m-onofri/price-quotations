aeac996e2b6c6cf16c24aec63e8ab78027db4e1c
Manuele Onofri <manuele.onofri@gmail.com>
2019-04-07 15:37:10 +0200

    Updates

---
+++ precache-manifest.7f32e51d437a92e1638951f62ab5425e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3488cc09f0129ea43d71",
+    "url": "/price-quotations/static/css/main.a5d7008d.chunk.css"
+  },
+  {
+    "revision": "3488cc09f0129ea43d71",
+    "url": "/price-quotations/static/js/main.8ad57bff.chunk.js"
+  },
+  {
+    "revision": "ce5b47678472c93e4f56",
+    "url": "/price-quotations/static/js/runtime~main.ac43ef1b.js"
+  },
+  {
+    "revision": "17845f40ab3b7ba2eb7c",
+    "url": "/price-quotations/static/js/2.86104ed0.chunk.js"
+  },
+  {
+    "revision": "357dfc318c276a3c4f56f7c6fe94e1aa",
+    "url": "/price-quotations/index.html"
+  }
+];
