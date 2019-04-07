6cf479fbeb39a7e0b9f22075917221dbfde5cd1d
Manuele Onofri <manuele.onofri@gmail.com>
2019-04-07 12:03:02 +0200

    Add proptypes to TotalAmount component

--- src/components/TotalAmount.js
+++ src/components/TotalAmount.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const TotalAmount = props => <h1 id="totalAmount">Total: {props.total} €</h1>
+
+TotalAmount.propTypes = {total: PropTypes.number.isRequired}
 
 export default TotalAmount;
